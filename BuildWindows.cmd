196f91343f98fe98590729a7f2a717c8be7d6506
Andrea <gtburraco@gmail.com>
2026-04-22 23:49:42 +0200

    Delete BuildWindows.cmd

--- BuildWindows.cmd
+++
@@ -1,2 +0,0 @@
-pyinstaller main.py --windowed --icon Icons\ico\icon.ico --onefile  --add-data "Icons\svg;Icons\svg" --add-data "Icons\ico;Icons\ico" --name "Bingo-Tournaments" --splash "images\Splash.png"
-pause
